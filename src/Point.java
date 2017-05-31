d2b3cdfb999eedf57ea23fe29c2ab13aeb9ed207
Asanhik <daulbaev.2011@mail.ru>
2017-05-31 18:59:04 +0600

    Added to class Point

--- src/Point.java
+++ src/Point.java
@@ -1,5 +1,39 @@
-/**
- * Created by пользователь on 30.05.2017.
- */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Point {
+
+    private double x = 0;
+    private double y = 0;
+    private int cluster_number = 0;
+
+    public Point(double x, double y) {
+        this.setX(x);
+        this.setY(y);
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public void setCluster(int n) {
+        this.cluster_number = n;
+    }
+
+    public int getCluster() {
+        return this.cluster_number;
+    }
 }
